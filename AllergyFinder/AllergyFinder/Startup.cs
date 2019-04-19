4e5d5b28488a3878359e88519a792ec7a99e55a7
jarydc333@gmail.com <jarydc333@gmail.com>
2019-04-19 13:05:56 -0500

    Initial push to add AllergFinder to git

---
+++ AllergyFinder/AllergyFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllergyFinder.Startup))]
+namespace AllergyFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
