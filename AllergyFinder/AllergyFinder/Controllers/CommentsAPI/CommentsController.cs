bef8745b6966d988761e644030d3147f66fddb38
jarydc333@gmail.com <jarydc333@gmail.com>
2019-04-23 10:32:17 -0500

    The application now has a working web API to get all current comments as well as the ability to show the lat and long of a restaurant once it has been searched

---
+++ AllergyFinder/AllergyFinder/Controllers/CommentsAPI/CommentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AllergyFinder.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AllergyFinder.Controllers.CommentsAPI
+{
+    [RoutePrefix("api/comments")]
+    public class CommentsController : ApiController
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        [HttpGet]
+        public List<LocationComment> GetComments()
+        {
+            var comments = db.LocationComments.ToList();
+            return comments;
+        }
+        
+    }
+}
